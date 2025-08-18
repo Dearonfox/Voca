789420f854b81b8d9d13e35a2b9953d8988b2ac5
Dearonfox <hjklpo909@naver.com>
2025-08-18 18:37:31 +0900

    crud 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from "./component/Header";
 import Day from "./component/Day"
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord";
 import { useState } from 'react';
+import CreateDay from "./component/CreateDay";
 function App() {
   return (
     <BrowserRouter>
@@ -15,6 +17,12 @@ function App() {
             </Route>
             <Route exact path ="/day/:day">
               <Day />
+            </Route>
+            <Route path ="/create_word">
+              <CreateWord />
+            </Route>
+            <Route path ="/create_day">
+              <CreateDay />
             </Route>
             <Route>
               <EmptyPage />
