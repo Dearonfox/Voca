96d5f099b5026f0258839d2ea9abb8367f0c59fc
Dearonfox <hjklpo909@naver.com>
2025-08-29 18:56:14 +0900

    Ui,Ux 업데이트

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className ="header">
             <h1>
-                <Link to ="/">토익 영단어</Link>
+                <Link to ="/">  토익 영단어</Link>
             </h1>
             <div className = "menu">
                 <Link to ="/create_word" className ="link">
